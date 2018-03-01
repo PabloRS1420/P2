1a78cae858a6d9ad507ce3ca392c8e88bc9bc843
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-01 14:20:35 +0100

    Create stack_element.h

---
+++ stack_element.h
@@ -0,0 +1,16 @@
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct _Stack Stack;
+
+stack *stack_ini();
+void stack_destroy(stack *);
+Status stack_push(stack *, const Element *);
+Element stack_pop(stact *);
+Element stack_top(const stact *);
+Bool stack_isEmpty(const stack *);
+Bool stack_isEmpty(const stack *);
+int stack_print(FILE *, const stack *);
+
+#endif
+
