d04b3b31060acbe56de667537a15b4b31781cd2b
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-01 14:49:23 +0100

    Update p2_e1.c

--- p2_e1.c
+++ p2_e1.c
@@ -4,4 +4,31 @@
 #include "types.h"
 
 int main(){
+    int x, y, i;
+    stack *sTotal = NULL;
+    stack *sImpar = NULL;
+    stack *sPar = NULL;
+    FILE *f;
+    sTotal = stack_ini();
+    sImpar = stack_ini();
+    sPar = stack_ini();
+    printf("> ./p2_e1 %d", x);
+    
+    printf("Pila total (no llena, vacia):");
+    for(i=0; i<x; i++){
+        printf("\nIntroduce número: ");
+        scanf("%d", &y);
+        stack_push(sTotal, y);
+    }
+    
+    printf("Pila total (no llena, no vacia):");   
+    while(stack_isEmpty==FALSE){
+        if(stack_top%2){
+            stack_push(sPar, stack_pop(sTotal));
+        }
+        else{
+            stack_push(sPar, stack_pop(sTotal));
+        }
+    }
+    
 }
