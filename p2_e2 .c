7a6205a430b1de45c750ea55b59be93079426ebb
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-08 13:50:25 +0100

    Update p2_e2 .c

--- p2_e2 .c
+++ p2_e2 .c
@@ -4,3 +4,12 @@
 #include "types.h"
 
 int main(int argc, char * argv[]){
+   int y, i;
+   stack *sTotal = NULL;
+   stack *sImpar = NULL;
+   stack *sPar = NULL;
+   FILE *f;
+   sTotal = stack_ini();
+   sImpar = stack_ini();
+   sPar = stack_ini();
+   printf("> ./p2_e2 %d", argv[1]);
