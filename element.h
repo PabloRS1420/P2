c4195eea2fcc8404536de35346ad0f394d13d3db
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-08 14:12:47 +0100

    Update element.h

--- element.h
+++ element.h
@@ -6,7 +6,7 @@ typedef struct _Stack Stack;
 Element *element_ini();
 void element_destroy(element *);
 Status element_setInfo(element *, void *);
-void element_getInfo(element *);
+Point element_getInfo(element *);
 Element *element_copy(const element *);
 Bool element_equals(const element *, const element *);
 int element_print(FILE *, const element *);
