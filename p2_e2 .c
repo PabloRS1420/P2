ffcb0886b8642b3f0c3c1e602e282270391ac330
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-08 14:50:31 +0100

    Update p2_e2 .c

--- p2_e2 .c
+++ p2_e2 .c
@@ -4,12 +4,37 @@
 #include "types.h"
 
 int main(int argc, char * argv[]){
-   int y, i;
-   stack *sTotal = NULL;
-   stack *sImpar = NULL;
-   stack *sPar = NULL;
-   FILE *f;
-   sTotal = stack_ini();
-   sImpar = stack_ini();
-   sPar = stack_ini();
-   printf("> ./p2_e2 %d", argv[1]);
+	
+	printf("> ./p2_e2 "%s"", argv[1]);
+    Stack *s = NULL;
+	Element *e = NULL;
+	Point *p = NULL;
+    s = stack_ini();
+	e = element_ini();
+	p = point_ini();
+    FILE *fp = fopen("argv[1]", "r+");
+    int nFil, nCol, i, j;
+	char symbol;
+	fscanf(fp, "%d %d", &nFil, &nCol);
+	
+    for(i=0; i<nFil; i++){
+		for(j=0; i<nCol; j++) {
+			fscanf(fp, "%s", symbol);
+			point_setCoordinateX(p, i);
+			point_setCoordinateY(p, j);
+			point_setSymbol(p, symbol);
+			element_setInfo(e, p);
+			stack_push(s, e);
+		}
+	}
+	
+	for(; i>=0; i--){
+		for(; j>=0, j--){
+			e = stack_pop(s);
+			p = element_getInfo(e);
+			point_print(fp, p);
+		}
+	}
+   
+   fclose(fp);
+}
