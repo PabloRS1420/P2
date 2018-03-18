a5fb0dde881fdde07a28fc3c6ec9f27d4ba752e9
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-18 14:37:48 +0100

    Update p2_e2 .c

--- p2_e2 .c
+++ p2_e2 .c
@@ -8,15 +8,15 @@
 int main(int argc, char * argv[]){
     printf("> ./p2_e2 "%s"", argv[1]);
     Stack *s = NULL;
-	Element *e = NULL;
-	Point *p = NULL;
-   	s = stack_ini();
-	e = element_ini();
-	p = point_ini();
-   	FILE *fp = fopen("argv[1]", "r+");
-   	int nFil, nCol, i, j;
-	char symbol;
-	fscanf(fp, "%d %d", &nFil, &nCol);
+    Element *e = NULL;
+    Point *p = NULL;
+    s = stack_ini();
+    e = element_ini();
+    p = point_ini();
+    FILE *fp = fopen("argv[1]", "r+");
+    int nFil, nCol, i, j;
+    char symbol;
+    fscanf(fp, "%d %d", &nFil, &nCol);
 	
     for(i=0; i<nFil; i++){
 		for(j=0; i<nCol; j++) {
