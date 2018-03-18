784f403b981526ba50100882afc39c39db3fab26
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-18 14:36:27 +0100

    Update p2_e2 .c

--- p2_e2 .c
+++ p2_e2 .c
@@ -6,7 +6,7 @@
 #include "types.h"
 
 int main(int argc, char * argv[]){
-	printf("> ./p2_e2 "%s"", argv[1]);
+    printf("> ./p2_e2 "%s"", argv[1]);
     Stack *s = NULL;
 	Element *e = NULL;
 	Point *p = NULL;
