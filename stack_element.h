41d327f6c67f22ea7468f0a0577608166a6c7f01
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-20 17:05:13 +0100

    Update stack_element.h

--- stack_element.h
+++ stack_element.h
@@ -4,13 +4,13 @@
 typedef struct _Stack Stack;
 
 stack *stack_ini();
-void stack_destroy(stack *s);
-Status stack_push(stack *s, const Element *e);
-Element stack_pop(stact *s);
-Element stack_top(const stact *s);
-Bool stack_isEmpty(const stack *s);
-Bool stack_isEmpty(const stack *s);
-int stack_print(FILE *f, const stack *s);
+void stack_destroy(Stack *s);
+Status stack_push(Stack *s, const Element *e);
+Element stack_pop(Stack *s);
+Element stack_top(const Stack *s);
+Bool stack_isEmpty(const Stack *s);
+Bool stack_isEmpty(const Stack *s);
+int stack_print(FILE *f, const Stack *s);
 
 #endif
 
