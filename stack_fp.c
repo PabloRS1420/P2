fa4faabd5355f61933c6d46d3a1e8f5fa02da02b
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-20 17:11:57 +0100

    Update stack_fp.c

--- stack_fp.c
+++ stack_fp.c
@@ -11,7 +11,44 @@ struct _StackF {
     destroy_element_function_type   destroy_element_function; 
     copy_element_function_type      copy_element_function; 
     print_element_function_type     print_element_function; 
-}; 
+};
+
+StackF *stackF_ini(destroy_element_function_type f1, copy_element_function_type f2, print_element_function_type f3){
+    stackF *sF = NULL;
+    sF = (stackF *) malloc(siceof(stackF));
+    if(!sF) return NULL;
+    
+    
+    return sF;
+}
+
+void stackF_destroy(StackF *s){
+    
+}
+
+Status stackF_push(StackF *s, const void *v){
+    
+}
+
+void *stackF_pop(StackF *s){
+    
+}
+
+void *stackF_top(const StackF *s){
+    
+}
+
+Bool stackF_isEmpty(const StackF *s);{
+    
+}
+
+Bool stackF_isFull(const StackF *s){
+    
+}
+
+int stackF_print(FILE *f, const StackF *s){
+    
+}
 
 
 
