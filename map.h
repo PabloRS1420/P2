a2dd8181271d5af0d8063bdc62d1996e19e12de9
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-20 17:07:26 +0100

    Update map.h

--- map.h
+++ map.h
@@ -12,15 +12,15 @@ typedef enum {
 typedef struct _Map Map;
 
 Map *map_ini ();
-void map_destroy(map *map);
+void map_destroy(Map *map);
 int map_getNrows(const Map *map);
-int map_getNcols(const map *map);
+int map_getNcols(const Map *map);
 Point *map_getInput(const Map *map);
 Point *map_getOutput(const Map *map);
 Point * map_getNeighborPoint(const Map * map, const Point *p, const Move mov);
 Status map_setSize(Map *map, int row, int col);
 Status map_setPoint(Map *map, const Point *p);
-int map_print(FILE *f, const map *map);
+int map_print(FILE *f, const Map *map);
 Point deep_search(Map *map, Point *p);
 
 #endif
